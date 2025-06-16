556600e5410c6099127762aea98186d9872116b4
Mahipat Gami <mahipat.gami@promptdairytech.in>
2025-06-16 10:41:00 +0530

    Minor Changes

--- src/BloggingUI/src/app/components/sidebar/sidebar.component.ts
+++ src/BloggingUI/src/app/components/sidebar/sidebar.component.ts
@@ -11,25 +11,16 @@ import { RouterModule } from '@angular/router';
 })
 export class SidebarComponent {
   menuItems: { label: string, path: string }[] = [];
-  @Input() closed: boolean = false;
+  @Input() hideSidebar: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
-    //const roles = this.authService.getRoles(); // e.g., ['Admin']
-
-    //if (roles.includes('Admin')) {
       this.menuItems = [
         { label: 'Dashboard', path: '/admin/dashboard' },
         { label: 'Manage Posts', path: '/admin/posts' },
         { label: 'Users', path: '/admin/users' }
       ];
-    //} else if (roles.includes('User')) {
-    //  this.menuItems = [
-    //    { label: 'Dashboard', path: '/user/dashboard' },
-    //    { label: 'My Posts', path: '/user/my-posts' },
-    //    { label: 'Create Post', path: '/user/create' }
-    //  ];
-    //}
+    
   }
 
   role = localStorage.getItem('role'); // For example
